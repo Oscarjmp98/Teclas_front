3544de4630e0f54fcc1432ae56df4a8c0b146eda
Oscar Medina Paz <oscar.medina01@unicatolica.edu.co>
2025-05-28 20:44:03 -0500

    Update api.js V.2

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
-const API_URL = "http://localhost:5000/api/datos";
-
+//const API_URL = "http://localhost:5000/api/datos";
+const API_URL = "https://tecla2-back.vercel.app/api/datos";
 export const obtenerDatos = async () => {
     const res = await fetch(API_URL);
     return await res.json();
